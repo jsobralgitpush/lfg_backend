ab1c7170401fbbe2a845233c1e09f3c733530c6e
ze <jose.sobral@poli.ufrj.br>
2023-08-25 13:20:41 -0300

    chore(Custom Attributes using EAV): Use EAV strategy to handle custom Proposal fields

--- proposal/models.py
+++ proposal/models.py
@@ -1,7 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Proposal(models.Model):
     name = models.CharField(max_length=100)
     document = models.CharField(max_length=20)
     status = models.CharField(max_length=20)
+
+
+class CustomAttribute(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class CustomAttributeValue(models.Model):
+    attribute = models.ForeignKey(CustomAttribute, on_delete=models.CASCADE)
+    proposal = models.ForeignKey(Proposal, on_delete=models.CASCADE)
+    value = models.TextField()
+
+    class Meta:
+        unique_together = (('attribute', 'proposal'),)
+
+    def __str__(self):
+        return f"{self.attribute.name}: {self.value}"
