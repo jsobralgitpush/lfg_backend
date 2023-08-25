e2dd969e2c286f18c76be60ed931370907989b82
ze <jose.sobral@poli.ufrj.br>
2023-08-25 13:24:32 -0300

    chore(Custom Admin Fields): Setup to create custom admin fields to Proposal

--- proposal/admin.py
+++ proposal/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Proposal, CustomAttribute, CustomAttributeValue
+
+
+class CustomAttributeValueInline(admin.TabularInline):
+    model = CustomAttributeValue
+    extra = 1
+
+
+@admin.register(Proposal)
+class ProposalAdmin(admin.ModelAdmin):
+    inlines = [CustomAttributeValueInline]
+
+
+@admin.register(CustomAttribute)
+class CustomAttributeAdmin(admin.ModelAdmin):
+    list_display = ('name',)
