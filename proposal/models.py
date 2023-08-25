63a2365a034f1f868077c83cf281219405399605
ze <jose.sobral@poli.ufrj.br>
2023-08-25 12:48:34 -0300

    chore(Proposal Model): Add proposal model to our app

--- proposal/models.py
+++ proposal/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Proposal(models.Model):
+    name = models.CharField(max_length=100)
+    document = models.CharField(max_length=20)
+    status = models.CharField(max_length=20)
