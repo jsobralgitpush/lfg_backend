3e2135a5f01fabf606a9c06fd1fcf902b0bee708
ze <jose.sobral@poli.ufrj.br>
2023-08-27 08:59:59 -0300

    chore(eav): Change eav strategy

--- proposal/admin.py
+++ proposal/admin.py
@@ -1,10 +1,1 @@
 from django.contrib import admin
-
-# Register your models here.
-from django.contrib import admin
-from .models import CustomAttribute
-
-
-@admin.register(CustomAttribute)
-class CustomAttributeAdmin(admin.ModelAdmin):
-    list_display = ('name',)
--- proposal/models.py
+++ proposal/models.py
@@ -7,22 +7,3 @@ class Proposal(models.Model):
     name = models.CharField(max_length=100)
     document = models.CharField(max_length=20)
     status = models.CharField(max_length=20)
-
-
-class CustomAttribute(models.Model):
-    name = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.name
-
-
-class CustomAttributeValue(models.Model):
-    attribute = models.ForeignKey(CustomAttribute, on_delete=models.CASCADE)
-    proposal = models.ForeignKey(Proposal, on_delete=models.CASCADE)
-    value = models.TextField()
-
-    class Meta:
-        unique_together = (('attribute', 'proposal'),)
-
-    def __str__(self):
-        return f"{self.attribute.name}: {self.value}"
