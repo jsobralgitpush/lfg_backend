e8f0fb9e7c0927469144fa51663ca995284388ce
ze <jose.sobral@poli.ufrj.br>
2023-08-27 12:05:00 -0300

    feat(eav): Add proposal attributes to admin

--- proposal/admin.py
+++ proposal/admin.py
@@ -1,1 +1,15 @@
 from django.contrib import admin
+from eav.forms import BaseDynamicEntityForm
+from eav.admin import BaseEntityAdmin
+from .models import Proposal
+
+
+class ProposalAdminForm(BaseDynamicEntityForm):
+    model = Proposal
+
+
+class ProposalAdmin(BaseEntityAdmin):
+    model = ProposalAdminForm
+
+
+admin.register(Proposal, ProposalAdmin)
