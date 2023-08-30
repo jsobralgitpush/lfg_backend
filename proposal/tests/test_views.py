f6a0c20f02e2e7378f43cf689631e342c3b27bdf
ze <jose.sobral@poli.ufrj.br>
2023-08-30 18:01:49 -0300

    test(proposal): Add ProposalList and ProposalRetrieve tests

--- proposal/tests/test_views.py
+++ proposal/tests/test_views.py
@@ -3,21 +3,56 @@ from rest_framework import status
 from rest_framework.test import APITestCase
 from eav.models import Attribute
 from proposal.serializers import AttributeSerializer
+from proposal.models import Proposal
 
 
 class ProposalAttributesViewTestCase(APITestCase):
     def setUp(self):
-        self.url = reverse('proposal:attributes')
         self.attribute1 = Attribute.objects.create(
-            name='Attribute 1', datatype=Attribute.TYPE_TEXT)
+            name='city', datatype=Attribute.TYPE_TEXT)
         self.attribute2 = Attribute.objects.create(
-            name='Attribute 2', datatype=Attribute.TYPE_TEXT)
+            name='address', datatype=Attribute.TYPE_TEXT)
+        Proposal.objects.create(name="Proposal 1", document="Document 1")
+        Proposal.objects.create(name="Proposal 2", document="Document 2")
 
-    def test_get_proposal_attributes(self):
-        response = self.client.get(self.url)
+    def test_list_proposal_attributes(self):
+        """
+        Ensure we can list all attributes.
+        """
+        url = reverse('proposal:attributes')
+        response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 2)
 
-        serializer = AttributeSerializer(
-            [self.attribute1, self.attribute2], many=True)
-        self.assertEqual(response.data, serializer.data)
+    def test_create_proposal(self):
+        """
+        Ensure we can create a new proposal object.
+        """
+        data = {'name': 'Test Name',
+                'document': 'Test Document', 'city': 'TestValue'}
+        url = reverse('proposal:list-create')
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Proposal.objects.count(), 3)
+
+    def test_proposal_list(self):
+        """
+        Ensure we can list all proposals.
+        """
+        url = reverse('proposal:list-create')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 2)
+        self.assertEqual(response.data[0]['name'], 'Proposal 1')
+        self.assertEqual(response.data[1]['name'], 'Proposal 2')
+
+    def test_retrieve_proposal(self):
+        """
+        Ensure we can retrieve a proposal.
+        """
+        proposal = Proposal.objects.create(
+            name='Test Name', document='Test Document')
+        url = reverse('proposal:retrieve', args=[proposal.id])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['name'], 'Test Name')
