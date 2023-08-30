4e9093fa14caa06a741f57e90387a9c5d898393f
ze <jose.sobral@poli.ufrj.br>
2023-08-30 16:40:25 -0300

    feat(proposal): Add proposal list logic

--- proposal/views.py
+++ proposal/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
 from rest_framework import mixins
 from rest_framework import generics
-from .serializers import AttributeSerializer
+from rest_framework import status
+from rest_framework.response import Response
+from .serializers import AttributeSerializer, ProposalSerializer
+from .models import Proposal
+from .tasks import send_proposal_task
 from eav.models import Attribute
 
 # Create your views here.
@@ -13,3 +16,32 @@ class ProposalAttributesView(mixins.ListModelMixin, generics.GenericAPIView):
 
     def get(self, request, *args, **kwargs):
         return self.list(request, *args, **kwargs)
+
+
+class ProposalList(generics.ListCreateAPIView):
+    queryset = Proposal.objects.all()
+    serializer_class = ProposalSerializer
+
+    def create(self, request, *args, **kwargs):
+        name = request.data.get('name')
+        document = request.data.get('document')
+        eav_data = {k: v for k, v in request.data.items() if k not in [
+            'name', 'document']}
+
+        proposal = Proposal(name=name, document=document)
+
+        for key, value in eav_data.items():
+            setattr(proposal.eav, f'{key.lower()}', f'{value}')
+
+        proposal.status = 'pending_by_system'
+
+        proposal.save()
+        send_proposal_task.delay(name, document, proposal.id)
+
+        serializer = self.get_serializer(proposal)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class ProposalRetrieveView(generics.RetrieveAPIView):
+    queryset = Proposal.objects.all()
+    serializer_class = ProposalSerializer
