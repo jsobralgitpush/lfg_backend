2de2a8f565ce8013065f140c253c0e1291671e54
ze <jose.sobral@poli.ufrj.br>
2023-08-31 11:00:57 -0300

    refactor(urls): Add django string regex to urls

--- proposal/urls.py
+++ proposal/urls.py
@@ -1,10 +1,10 @@
-from django.urls import path
-from .views import ProposalAttributesView, ProposalList, ProposalRetrieveView
+from django.urls import re_path
+from .views import ProposalList, ProposalRetrieveView, ProposalAttributesView
 
 app_name = 'proposal'
 
 urlpatterns = [
-    path('', ProposalList.as_view(), name='list-create'),
-    path('<int:pk>/', ProposalRetrieveView.as_view(), name='retrieve'),
-    path('attributes', ProposalAttributesView.as_view(), name='attributes'),
+    re_path(r'^$', ProposalList.as_view(), name='list-create'),
+    re_path(r'^(?P<pk>\d+)/$', ProposalRetrieveView.as_view(), name='retrieve'),
+    re_path(r'^attributes/$', ProposalAttributesView.as_view(), name='attributes'),
 ]
